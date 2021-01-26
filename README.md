c6e0a024e3f714b3f939e5ecf6df183cc104fa51
Victor Hripko <hripko.v@soft2bet.com>
2021-01-26 15:37:08 +0200

    done

--- README.md
+++ README.md
@@ -1,21 +1,7 @@
-# Template for FrontLoops [Elements] challenge
+# Paging
 
-Install all dependencies
-```bash
-npm i
-```
-
-For start watching all files use:
-```bash
-npm run start
-```
-
-then open in browser
-```bash
-http://localhost:1234/
-```
-
-For build all files use:
-```bash
-npm run build
-```
+Build a paging component.
+Whenever you click a load moore button - an AJAX request should be sent to the Star Wars API.
+Request URL for the first request is <https://swapi.dev/api/people/>
+Any other API can be used as long as it returns an array of items and url for next request.
+Items should be added with animation.
--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
 import './style.scss';
 
 (() => {
+  let API = 'https://swapi.dev/api/people/';
+
+  const loadBtn = document.querySelector('.load-cards');
+  const wrapper = document.querySelector('.cards');
+
+  loadBtn.addEventListener('click', () => {
+    loadBtn.disabled = true;
+    loadBtn.classList.add('btn--loading');
+
+    getCards(() => {
+      loadBtn.disabled = false;
+      loadBtn.classList.remove('btn--loading');
+    });
+  });
+
+
+  function getCards(callback) {
+    fetch(API)
+      .then(res => res.json())
+      .then(data => {
+        data.next ? API = data.next : loadBtn.style.display = 'none';
+        renderCards(data.results);
+      })
+      .finally(callback);
+  };
+
+  function renderCards(items) {
+    items.forEach((el, idx) => {
+      wrapper.insertAdjacentHTML('beforeend', `<div class="card-item" style="animation-delay: ${idx * 10}ms">${el.name}</div>`)
+    });
+
+    wrapper.style.height = wrapper.scrollHeight + 'px';
+  };
+
 
 })();
